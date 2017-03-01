0b6a17796266daca2fb6ae6d23f50b8ea54802c4
Vinay singh <vsingh@grepruby.com>
2017-03-01 12:41:28 +0530

    Add modal for user profile

---
+++ feb-17/week-4/ProfilePage/app/src/main/java/com/example/rails/profilepage/modal/User.java
@@ -0,0 +1,53 @@
+package com.example.rails.profilepage.modal;
+
+public class User {
+    private String name;
+    private String email;
+    private String mobile;
+    private String dob;
+    private String gender;
+
+    public User() {
+      /*Blank default constructor essential for Firebase*/
+    }
+    //Getters and setters for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Getters and setters for email
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //Getters and setters for mobile
+    public String getMobile() {
+        return mobile;
+    }
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+
+    //Getters and setters for dob
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String bloodGroup) {
+        this.dob = dob;
+    }
+
+    //Getters and setters for gender
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
