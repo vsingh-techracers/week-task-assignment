4cf442642ce7c07a133998b482a51a076fe685b7
Vinay singh <vsingh@grepruby.com>
2017-03-02 11:34:31 +0530

    Update base fragment

--- feb-17/week-4/ProfilePage/app/src/main/java/com/example/rails/profilepage/base/BaseFragment.java
+++ feb-17/week-4/ProfilePage/app/src/main/java/com/example/rails/profilepage/base/BaseFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.rails.profilepage.MainActivity;
 
@@ -25,6 +31,40 @@ public abstract class BaseFragment extends Fragment {
 		super.onResume();
 		getActivity().setTitle(getTitle());
 	}
+
+	public void hideIt(LinearLayout ll){
+		ll.setVisibility(View.GONE);
+	}
+
+	public void setErrorMsg(LinearLayout linearLayout, TextView tv, String msg){
+		linearLayout.setVisibility(View.VISIBLE);
+		tv.setText(msg);
+	}
+
+    public String getStringDataFromEditText(EditText v){
+        return v.getText().toString().trim();
+    }
+
+    public String getStringDataFromSpinner(Spinner v){
+        return v.getSelectedItem().toString();
+    }
+
+    public String getStringDataFromRadioButton(RadioButton v){
+        return v.getText().toString();
+    }
+
+    public boolean isNameValid(String name){
+        return name.matches(Constants.nameRegex);
+    }
+    public boolean isEmailValid(String email){
+        return email.matches(Constants.emailRegex);
+    }
+    public boolean isPhoneValid(String phone){
+        return phone.matches(Constants.mobRegex);
+    }
+    public boolean isDateValid(String date){
+        return date.matches(Constants.dateRegex);
+    }
 
 	protected abstract String getTitle();
 }
