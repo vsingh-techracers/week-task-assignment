7b9d744af44403cefa22166f857e45b742e8458b
Vinay singh <vsingh@grepruby.com>
2017-03-03 04:11:05 +0530

    Update constants file for profile fragment

--- feb-17/week-4/ProfilePage/app/src/main/java/com/example/rails/profilepage/base/Constants.java
+++ feb-17/week-4/ProfilePage/app/src/main/java/com/example/rails/profilepage/base/Constants.java
@@ -9,10 +9,10 @@ public class Constants {
     public static final String emailRegex = "^[\\p{L}0-9!$'*+\\-_]+(\\.[\\p{L}0-9!$'*+\\-_]+)*@[\\p{L}0-9]+(\\-[\\p{L}0-9]+)*(\\.[\\p{L}0-9]+)*(\\.[\\p{L}]{2,})$";
     public static final String mobRegex = "^[0-9]{10}$";
     public static final String dateRegex = "^[0-3]{1}+[0-9]{1}/[0-1]{1}+[0-9]{1}/[0-9]{4}$";
-    public static final String kProfilePageFragment = "User profile";
     public static final String nameErrorText = "Name is not valid!";
     public static final String emailErrorText = "Email is not valid!";
     public static final String mobErrorText = "Mobile no is not valid!";
     public static final String dateErrorText = "Please enter valid date!";
     public static final String genderErrorText = "Unknown identity!";
+    public static final String kProfilePageFragment = "Edit profile";
 }
