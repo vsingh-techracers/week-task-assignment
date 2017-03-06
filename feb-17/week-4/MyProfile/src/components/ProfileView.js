096e7c73332ed3b533229315551f24cdf5fc4c9d
Vinay singh <vsingh@grepruby.com>
2017-03-06 18:38:19 +0530

    Update profile view style and ui

--- feb-17/week-4/MyProfile/src/components/ProfileView.js
+++ feb-17/week-4/MyProfile/src/components/ProfileView.js
@@ -10,13 +10,19 @@ import {
   ActionSheetIOS,
   Modal,
   ListView,
+  Image,
+  ScrollView,
+  TextInput,
   TouchableOpacity,
   ActivityIndicator,
   Dimensions,
   AsyncStorage,
   Navigator,
 } from 'react-native'
-const { width, height } = Dimensions.get('window');
+import { ProfileStyles } from "../styles/ProfileStyles";
+import { EditProfileStyle } from "../styles/EditProfileStyle";
+import variables from "../styles/StyleVariables";
+const { width, height } = Dimensions.get("window");
 
 export default class ProfileView extends Component {
 
@@ -27,6 +33,24 @@ export default class ProfileView extends Component {
     }
   }
 
+  renderUserAttr() {
+    return user.map((attribute, index)=>
+      <View key = {index} style = {{ margin: variables.mediumDistance }} >
+        <View style = {[ProfileStyles.textContainer, ProfileStyles.bodyTextContainer]} >
+          <Text style = {ProfileStyles.text} >
+            Male
+          </Text>
+          <TouchableOpacity >
+            <Image
+              source={require('../images/edit_icon.png')}
+              style = {ProfileStyles.editIcon} />
+          </TouchableOpacity>
+        </View>
+        <View style = {ProfileStyles.lightSeparater}/>
+      </View>
+    );
+  }
+
   render() {
     return (
       <Navigator
@@ -35,216 +59,88 @@ export default class ProfileView extends Component {
     )
   }
 
-  renderScene(route,navigator) {
-    let iconPath = this.state.avatarSource === '' ? require('../images/profile.png') : this.state.avatarSource
-    let maleButtonBackground = (this.state.gender === 'Male' ? {backgroundColor: 'white'} : {backgroundColor: 'transparent'})
-    let femaleButtonBackground = (this.state.gender === 'Female' ? {backgroundColor: 'white'} : {backgroundColor: 'transparent'})
-    return(
-         
-    <Image style = {styles.backgroundImage} source={require('../images/main_background.jpg')}>
-
-      <View style={styles.profileImageHolderView}>
-        <TouchableOpacity style={styles.profileImageClickableStyle}
-          onPress={() => this.imagepicker()} >
-
-          <Image source={iconPath} style = {styles.profileImageStyle}/>
-
-        </TouchableOpacity>
-      </View>
-
-      <ScrollView style={{flex:1}}>
-        <View style={{marginTop: 20}}>
-          <View style={styles.inputViewStyle}>
-            <Image source={require('../images/User.png')} style={styles.smallIconStyle}/>
-            <TextInput
-              ref='1'
-              style={styles.textFieldStyle}
-              placeholder='Name'
-              returnKeyType='next'
-              underlineColorAndroid='transparent'
-              placeholderTextColor='rgba(230,230,230,0.5)'
-              selectionColor={'white'}
-              onChangeText={(nametext) => this.setState({nametext})}
-              value={this.state.nametext}
-              onSubmitEditing={(event)=>{this.refs.email.focus()}}
-            />
+  renderScene(route, navigator) {
+    return (
+      <View style = {ProfileStyles.container} >
+        <View style = {ProfileStyles.header} >
+          <View style = {ProfileStyles.editIconContainer}>
+            <TouchableOpacity
+              onPress ={()=> this.props.navigator.push({id: "editProfile"})}
+            >
+              <Image
+                source={require('../images/edit_icon.png')}
+                style = {EditProfileStyle.editIcon} />
+            </TouchableOpacity>
           </View>
-          <View style={styles.inputViewStyle}>
-            <Image source={require('../images/Mail.png')} style={styles.smallIconStyle}/>
-            <TextInput
-              ref='1'
-              style={styles.textFieldStyle}
-              placeholder='Email'
-              keyboardType='email-address'
-              returnKeyType='next'
-              ref='email'
-              underlineColorAndroid='transparent'
-              placeholderTextColor='rgba(230,230,230,0.5)'
-              selectionColor={'white'}
-              onChangeText={(emailtext) => this.setState({emailtext})}
-              value={this.state.emailtext}
-              onSubmitEditing={(event)=>{this.refs.phoneNumber.focus()}}
-            />
+          <View style = {ProfileStyles.profilePicContainer} >
+            <TouchableOpacity style={ProfileStyles.profileImgBlock} >
+              <Image
+                source={require('../images/profile.png')}
+                style = {ProfileStyles.profileImage} />
+            </TouchableOpacity>
           </View>
-          <View style={styles.inputViewStyle}>
-            <Image source={require('../images/Phone.png')} style={styles.smallIconStyle}/>
-            <TextInput
-              ref='1'
-              style={styles.textFieldStyle}
-              placeholder='Phone Number'
-              keyboardType='phone-pad'
-              ref='phoneNumber'
-              underlineColorAndroid='transparent'
-              placeholderTextColor='rgba(230,230,230,0.5)'
-              selectionColor={'white'}
-              onChangeText={(phonenumber) => this.setState({phonenumber})}
-              value={this.state.phonenumber}
-              onSubmitEditing={(event)=>{}}
-            />
+          <View style = {ProfileStyles.usernameContainer} >
+            <Text style = {ProfileStyles.username} >
+              Vinay Singh
+            </Text>
           </View>
-          <View style={styles.datePickerViewStyle}>
-            <DatePicker
-              style={{width: 205}}
-              date={this.state.date}
-              mode="date"
-              placeholder="select date"
-              format="MMM DD YYYY"
-              
-              confirmBtnText="Confirm"
-              cancelBtnText="Cancel"
-              iconSource={require('../images/Calendar.png')}
-              customStyles={{
-                dateIcon: {
-                  position: 'absolute',
-                  left: 0,
-                  marginTop: 8,
-                  marginLeft: 20,
-                  width: 25,
-                  height: 25
-                },
-                dateInput: {
-                  marginLeft: 20,
-                  marginTop: 7,
-                  borderColor: '#FFFFFF00'
-                },
-                dateText: {
-                  fontSize: 17.0,
-                  color: '#FFFFFF',
-                  fontFamily: 'Avenir-Black'
-                },
-                btnTextConfirm: {
-                  color: 'rgb(0,122,255)'
-                }
-                
-              }}
-              onDateChange={(date) => {
-                Keyboard.dismiss()
-                this.setState({date: date})
-              }}
-            />
-          </View>
-
-          <View style={styles.inputViewStyle}>
-            <Image source={require('../images/Gender.png')} style={styles.smallIconStyle}/>
-            <View style={{flex:1,flexDirection: 'row',alignItems:'center',justifyContent:'center'}}>
-              <TouchableOpacity style={{flexDirection: 'row'}} onPress={() => this.setMaleGender()}>
-                <TouchableOpacity style={[{width: 25,height: 25,borderRadius: 13.5,alignItems:'center',justifyContent:'center',borderWidth:1,borderColor:'#FFFFFF',marginTop: 20,marginLeft: 10},maleButtonBackground]}
-                  onPress={() => this.setMaleGender()} >
-                
-                </TouchableOpacity>
-                <Text style={{marginTop: 20,fontSize: 20.0,color: '#FFFFFF',fontFamily: 'Avenir-Black',marginLeft: 10}}>Male</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={{flexDirection: 'row'}} onPress={() => this.setFemaleGender()}>
-                <TouchableOpacity style={[{width: 25,height: 25,borderRadius: 13.5,alignItems:'center',justifyContent:'center', borderWidth:1,borderColor:'#FFFFFF',marginTop: 20, marginLeft: 50},femaleButtonBackground]}
-                  onPress={() => this.setFemaleGender()} >
-                
-              </TouchableOpacity>
-              <Text style={{marginTop: 20,fontSize: 20.0,color: '#FFFFFF',fontFamily: 'Avenir-Black',marginLeft: 10}}>Female</Text>
-              </TouchableOpacity>
-
-            </View>
-          </View>
-
+          <View style = {ProfileStyles.blackSeparater}/>
         </View>
-      </ScrollView>
-      <View style={{alignItems:'center',justifyContent:'center', height: 55 }}>
-        <TouchableOpacity underlayColor='gray' style={{width: width-40,height: 40,alignItems:'center',justifyContent:'center', borderWidth:1,borderColor:'#FFFFFF',borderRadius: 10, marginBottom: 10,backgroundColor:'#FFFFFF'}}
-              onPress={() => this.onSubmitPressed()} >
-
-          <Text style={styles.submitButtonText}>Submit</Text>
-
-        </TouchableOpacity>
+        <ScrollView style = {ProfileStyles.body} >
+          <View style = {{ margin: variables.mediumDistance }} >
+            <View style = {[ProfileStyles.textContainer, ProfileStyles.bodyTextContainer]} >
+              <Text style = {ProfileStyles.text} >
+                Male
+              </Text>
+              <TouchableOpacity >
+                <Image
+                  source={require('../images/single_edit.png')}
+                  style = {ProfileStyles.editIcon} />
+              </TouchableOpacity>
+            </View>
+            <View style = {ProfileStyles.lightSeparater}/>
+          </View>
+          <View style = {{ margin: variables.mediumDistance }} >
+            <View style = {[ProfileStyles.textContainer, ProfileStyles.bodyTextContainer]} >
+              <Text style = {ProfileStyles.text} >
+                +91 9753238059
+              </Text>
+              <TouchableOpacity >
+                <Image
+                  source={require('../images/single_edit.png')}
+                  style = {ProfileStyles.editIcon} />
+              </TouchableOpacity>
+            </View>
+            <View style = {ProfileStyles.lightSeparater}/>
+          </View>
+          <View style = {{ margin: variables.mediumDistance }} >
+            <View style = {[ProfileStyles.textContainer, ProfileStyles.bodyTextContainer]} >
+              <Text style = {ProfileStyles.text} >
+                01 Aug, 1990
+              </Text>
+              <TouchableOpacity >
+                <Image
+                  source={require('../images/single_edit.png')}
+                  style = {ProfileStyles.editIcon} />
+              </TouchableOpacity>
+            </View>
+            <View style = {ProfileStyles.lightSeparater}/>
+          </View>
+          <View style = {{ margin: variables.mediumDistance }} >
+            <View style = {[ProfileStyles.textContainer, ProfileStyles.bodyTextContainer]} >
+              <Text style = {ProfileStyles.text} >
+                vinay@gmail.com
+              </Text>
+              <TouchableOpacity >
+                <Image
+                  source={require('../images/single_edit.png')}
+                  style = {ProfileStyles.editIcon} />
+              </TouchableOpacity>
+            </View>
+            <View style = {ProfileStyles.lightSeparater}/>
+          </View>
+        </ScrollView>
       </View>
-    </Image>
-
-      
     )
   } 
 }
-
-const styles = StyleSheet.create({
-
-  backgroundview: {
-    flex: 1,
-    resizeMode: 'cover', 
-  },
-  backgroundImage: {
-    flex: 1,
-    width : width,
-    height: height
-  },
-  profileImageHolderView: {
-    marginTop: 5,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  profileImageClickableStyle: {
-    alignItems:'center',
-    justifyContent:'center',
-    marginTop:20,
-    height: 140,
-    width: 140
-  },
-  profileImageStyle: {
-    width :140,
-    height: 140,
-    borderRadius:70
-  },
-  inputViewStyle: {
-    flexDirection: 'row',
-    alignItems:'center',
-    justifyContent:'center',
-    marginTop: 10
-  },
-  smallIconStyle: {
-    marginTop: 10,
-    marginLeft: 15,
-    width :30,
-    height: 30
-  },
-  textFieldStyle: {
-    flex: 1,
-    marginLeft:15,
-    marginRight:10,
-    paddingLeft:5,
-    paddingRight:5,
-    height: 40,
-    marginTop:10,
-    fontSize: 17.0,
-    color: '#FFFFFF',
-    fontFamily: 'Avenir-Black'
-  },
-  datePickerViewStyle: {
-    flexDirection: 'row',
-    alignItems:'flex-start',
-    justifyContent:'flex-start',
-    marginTop: 10
-  },
-  submitButtonText: {
-    marginTop: 0,
-    fontSize: 20.0,
-    color: 'rgb(21,53,75)',
-    fontFamily: 'Avenir-Black'
-  }
-});
---
+++ feb-17/week-4/MyProfile/src/styles/ProfileStyles.js
@@ -0,0 +1,100 @@
+/* @flow */
+
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+
+import variables from "./StyleVariables";
+const {height, width} = Dimensions.get("window");
+
+var ProfileStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: variables.navBarHeight
+  },
+  editIconContainer: {
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    padding: 10
+  },
+  header: {
+    height: 200,
+    justifyContent: "center",
+    alignSelf: "stretch",
+  },
+  profilePicContainer: {
+    flex: 1,
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  profileImgBlock: {
+    height: 134,
+    width: 134,
+    borderRadius:67,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    backgroundColor: "lightgrey"
+  },
+  profileImage: {
+    height: 130,
+    width: 130,
+    borderRadius:65
+  },
+  usernameContainer: {
+    height: 40,
+    alignSelf: "stretch",
+    justifyContent: "center"
+  },
+  username: {
+    alignSelf: "center",
+    fontSize: 18,
+    color: "black",
+    fontWeight: "500"
+  },
+  body: {
+    flex: 1,
+    alignSelf: "stretch"
+  },
+  textContainer: {
+    alignSelf: "stretch",
+    justifyContent: "flex-end"
+  },
+  bodyTextContainer: {
+    paddingBottom: 5,
+    flexDirection: "row"
+  },
+  text: {
+    flex: 1,
+    fontSize: 16,
+    color: "darkgrey",
+    fontWeight: "600"
+  },
+  editIcon: {
+    height: 20,
+    width: 20
+  },
+  blackSeparater: {
+    height: 1,
+    alignSelf: "stretch",
+    backgroundColor: "black",
+    margin: 0
+  },
+  whiteSeparater: {
+    height: 1,
+    alignSelf: "stretch",
+    backgroundColor: "white",
+    margin: 0
+  },
+  lightSeparater: {
+    height: 1,
+    alignSelf: "stretch",
+    backgroundColor: "lightgrey",
+    margin: 0
+  }
+});
+
+export { ProfileStyles };
