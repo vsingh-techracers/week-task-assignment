dd07b70ed1a2188c7e7a245eee6122d1dd28d8f4
Vinay singh <vsingh@grepruby.com>
2017-03-16 12:06:45 +0530

    Add constant style

---
+++ mar-17/week-2/rappi/App/src/constants/styles.js
@@ -0,0 +1,37 @@
+export const headerBGColor = '#000000';
+
+export const navBarIconSize = 24;
+export const backButtonMargin = 5;
+export const standardMargin = 5;
+
+export const textGreen = '#22A358';
+
+export const infoSectionImageSize = {
+  width: 100,
+  height: 150,
+};
+
+// FilterBar Constants
+export const filterBarBGColor = '#859bab';
+export const filterBarFontSize = 20;
+
+// ProductList Constants
+export const imageBGColor = '#000000';
+export const itemWidth = 180;
+export const itemHeight = 300;
+export const smokingDealBGColor = '#d05751';
+export const ratingsSectionBGColor = '#d79b00';
+export const onSaleTextColor = '#FFFFFF';
+export const GGTextColor = '#23a359';
+
+// ProductView Constants
+export const productViewImageWidth = 380;
+export const productViewImageHeight = 340;
+export const productViewImageWrapperHeight = 440;
+export const productViewShrinkedHeaderWidth = 250;
+export const footerHeight = 40;
+
+// TabBar Constants
+export const tabBarHeight = 30;
+export const activeTabBorderColor = '#1e90ff';
+export const tabItemBGColor = 'rgba(255, 255, 255, 0.7)';
